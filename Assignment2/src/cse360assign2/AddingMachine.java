047d5f24721e5e008bc1b4fe950ffee6137be1c9
SaudMSS <SaudMSS@172.20.10.2>
2019-06-18 00:19:21 -0700

    Initial version of Adding Machine.

---
+++ Assignment2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,50 @@
+package cse360assign2;
+
+public class AddingMachine {
+	private int total;
+	 
+	/**
+	 * Constructor to initialize variables
+	 */
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * Method to get total
+	 * @return
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * Method to add value into total
+	 * @param value
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * Method to subtract value from total
+	 * @param value
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * Metho to get string representation of total variable
+	 */
+	public String toString () {
+		return "";
+	}
+
+	/**
+	 * Method to clear all the variables values
+	 */
+	public void clear() {
+	
+	}
+}
